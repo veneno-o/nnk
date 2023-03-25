7ab7f2873fa3fef886a6444237a285c92105ca7f
chensong <1929750203@qq.com>
2023-03-25 11:15:50 +0800

    实现footer区域响应式

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -1,29 +1,34 @@
 <template>
     <footer>
         <div class="top" id="footer">
-            <div class="logo">Logo</div>
-            <div class="text">
-                <div class="left">
-                    <div class="title">快捷入口</div>
-                    <div class="ul">
-                        <a href="#">首页</a>
-                        <a href="#">关键技术</a>
-                        <a href="#">应用场景</a>
-                        <a href="#">应用价值</a>
-                        <a href="#">解决方案</a>
-                        <a href="#">关于我们</a>
+            <div class="left-text">
+                <div style="display: flex;justify-content: flex-end;">
+                <div class="logo">Logo</div>
+                <div class="text">
+                    <div class="left">
+                        <div class="title">快捷入口</div>
+                        <div class="ul">
+                            <a href="#">首页</a>
+                            <a href="#">关键技术</a>
+                            <a href="#">应用场景</a>
+                            <a href="#">应用价值</a>
+                            <a href="#">解决方案</a>
+                            <a href="#">关于我们</a>
+                        </div>
                     </div>
-                </div>
-                <div class="right">
-                    <div class="title">联系我们</div>
-                    <div class="ul">
-                        <p>咨询热线：15395713795</p>
-                        <p>简历投递：hr@nnkosmos.com</p>
-                        <p>公司地址：浙江省杭州市余杭区钱塘汇银中心6幢2单元</p>
-                        <p>北京市海淀区双清路双清大厦4号楼803</p>
+                    <div class="right">
+                        <div class="title">联系我们</div>
+                        <div class="ul">
+                            <p>咨询热线：15395713795</p>
+                            <p>简历投递：hr@nnkosmos.com</p>
+                            <p>公司地址：浙江省杭州市余杭区钱塘汇银中心6幢2单元</p>
+                            <p>北京市海淀区双清路双清大厦4号楼803</p>
+                        </div>
                     </div>
                 </div>
             </div>
+            </div>
+            <div></div>
             <div class="weixin">
                 <div class="left-weixin weixin-box">
                     <div>二维码</div>
