586a47fed8f5b6a0241e1606f8ea5b4f9edac56c
chensong <1929750203@qq.com>
2023-03-27 11:05:01 +0800

    长图片布局优化

--- src/components/LongImage/index.vue
+++ src/components/LongImage/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="longimage">
-        <div class="poster1">
+        <div style="width:100vw;">
+            <div class="poster1">
             <div class="text-content">
                 <h2 class="title1">TS数字人</h2>
                 <h2 class="title2">视频生成平台</h2>
@@ -15,6 +16,7 @@
                 <p class="content1">一键孪生数字人，多模态驱动视频内容创作</p>
                 <p class="content2">生成快、场景多、易操作、质量高</p>
             </div>
+        </div>
         </div>
     </div>
 </template>
