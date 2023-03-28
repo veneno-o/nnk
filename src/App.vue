9f79bd44fbffb1dbcb1cd5fdfb473da840d7bfbd
chensong <1929750203@qq.com>
2023-03-28 17:38:48 +0800

    动画效果解决方案确定，接下来进行尝试解决动画效果

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 import Menu from "@/components/Menu/index.vue";
 import Banner from "@/components/Banner/index.vue";
 import Footer from "@/components/Footer/index.vue";
@@ -15,7 +15,6 @@ import Carousel from "@/components/Carousel/index.vue";
     <Menu />
     <Banner />
     <Center>
-      <!-- <Accordion /> -->
     </Center>
 
     <div>
@@ -41,4 +40,42 @@ import Carousel from "@/components/Carousel/index.vue";
     padding-right: calc(var(--section-gap) / 2);
   }
 }
+</style> -->
+<script setup>
+import { ref, onMounted } from 'vue';
+const myRef = ref("");
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    // 判断当前元素是否可见
+    if (entry.isIntersecting) {
+      const dom = entry.target;
+      dom.style.marginTop = 100 + "px";
+      // 解除观察，有几张图片就触发几次
+      observer.unobserve(dom)
+    }
+  })
+})
+onMounted(() => {
+  observer.observe(myRef.value);
+});
+</script>
+
+<template>
+  <div class="view"></div>
+<div class="box" ref="myRef"></div>
+</template>
+
+<style>
+  .view{
+    width:100vw;
+    height:100vh;
+    background:red;
+  }
+  .box{
+    width:200px;
+    height:200px;
+    background:#000;
+    margin-top:200px;
+    transition:2s;
+  }
 </style>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,18 @@
+import { onMounted } from 'vue'
+
+const observer = new IntersectionObserver(entries => {
+  // console.log(3)
+  entries.forEach(entry => {
+    // 判断当前元素是否可见
+    if (entry.isIntersecting) {
+      const dom = entry.target;
+      dom.style.marginTop = (parseInt(dom.style.marginTop) - 20) + "px";
+      // 解除观察，有几张图片就触发几次
+      observer.unobserve(dom)
+    }
+  })
+})
+
 // 函数节流
 function throttle(func, delay) {
   let timer = null;
@@ -14,29 +29,8 @@ function throttle(func, delay) {
 }
 
 // 图片懒加载
-
-function image() {
+export function lazyLoad(dom) {
   onMounted(()=>{
-    console.log(1)
-    const btns = document.querySelectorAll("button")
-  
-    const observer = new IntersectionObserver(entries => {
-      console.log(3)
-      entries.forEach(entry => {
-        // 判断当前元素是否可见
-        if (entry.isIntersecting) {
-          const dom = entry.target;
-          dom.style.marginTop = (parseInt(dom.style.marginTop) - 20) + "px";
-          // 解除观察，有几张图片就触发几次
-          observer.unobserve(dom)
-        }
-      })
-    })
-  
-    btns.forEach(btn => {
-      console.log(2)
-      // 对每一个图片对象进行观察
-      observer.observe(btn)
-    })
+    observer.observe(dom);
   })
 }
