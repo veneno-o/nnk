8fea7eb7903f8e759aaac6a80c7b1910659183ee
chensong <1929750203@qq.com>
2023-04-01 18:44:14 +0800

    样式调整

--- src/utils/index.js
+++ src/utils/index.js
@@ -34,3 +34,45 @@ export function lazyLoad(dom) {
     observer.observe(dom);
   })
 }
+
+/**
+ * 
+<!-- <script setup>
+import { ref, onMounted } from 'vue';
+const myRef = ref("");
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    // 判断当前元素是否可见
+    if (entry.isIntersecting) {
+      const dom = entry.target;
+      dom.style.marginTop = 100 + "px";
+      // 解除观察，有几张图片就触发几次
+      observer.unobserve(dom)
+    }
+  })
+})
+onMounted(() => {
+  observer.observe(myRef.value);
+});
+</script>
+
+<template>
+  <div class="view"></div>
+<div class="box" ref="myRef"></div>
+</template>
+
+<style>
+  .view{
+    width:100vw;
+    height:100vh;
+    background:red;
+  }
+  .box{
+    width:200px;
+    height:200px;
+    background:#000;
+    margin-top:200px;
+    transition:2s;
+  }
+</style> -->
+ */
